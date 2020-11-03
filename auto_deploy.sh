7d35d3824f059e0ae7ad44ee9cbb962c5f863d5b
sigongweb <kisssulran@i-screammedia.com>
2020-11-03 09:02:05 +0900

    update

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -12,9 +12,9 @@ VERSION="lts"
 # 프로파일 이름(ex:stage,dev,pro)
 SPRING_PROFILE="stage"
 
+# 배포할 파일 생성 유무 확인
 check_app()
 {
-  # 배포할 파일, 배포할 파일의 디렉토리 생성 유무 확인
   if [[ -f ${APP_HOME}/${DEPLOY_FILE} ]];then
     return 0
   else
@@ -32,14 +32,11 @@ to_lowercase() {
 
 variable_func()
 {
-  # 파일명에서 확장자를 제거한 문자열을 서비스 이름으로 사용.
+# 파일명에서 확장자를 제거한 문자열을 서비스 이름으로 사용.
   SERVICE_NAME=$(echo ${DEPLOY_FILE%.*})
-  # 이미지 이름
+# 도커 이미지 이름
   IMAGE_NAME="${SERVICE_NAME}-${SPRING_PROFILE}"
-  # old 배포 파일 관련 변수(도커 배포 시 오래된 소스 파일은 삭제한다.)
-  OLD_FILE=$(ls -1 ${APP_HOME}/${SERVICE_NAME}*.jar 2> /dev/null | grep -v "$DEPLOY_FILE") # 디렉토리의 파일 이름 목록을 배열에 저장 (ls -1)
-  OLD_FILE_COUNT=$(ls ${APP_HOME}/${SERVICE_NAME}*.jar 2> /dev/null | grep -v "$DEPLOY_FILE" | wc -l)
-  # 도커 관련 변수
+# 도커 관련 변수
   CONTAINER_ID=$(docker ps -af ancestor=${IMAGE_NAME}:${VERSION} --format "{{.ID}}")
   IMAGE_ID=$(docker images -f=reference=${IMAGE_NAME}':*' --format "{{.ID}}")
 }
@@ -47,10 +44,9 @@ variable_func()
 # 대문자 deploy 파일을 소문자로 변경
 lowercase_deploy_file()
 {
-  ## app 파일명 추출
-  DEPLOY_FILE=$(basename $APP_HOME/*.jar)
-  local FILE=$(to_lowercase $DEPLOY_FILE) # 파일명을 소문자로 변경
-  if [[ $FILE != $DEPLOY_FILE ]];then     # 소문자로 변경한 파일과 원래 배포하려는 파일이 동일한지 비교
+  DEPLOY_FILE=$(basename $APP_HOME/*.jar)         # app 파일명 추출
+  local FILE=$(to_lowercase $DEPLOY_FILE)         # 파일명을 소문자로 변경
+  if [[ $FILE != $DEPLOY_FILE ]];then             # 소문자로 변경한 파일과 원래 배포하려는 파일이 동일한지 비교
     mv $DEPLOY_FILE $(to_lowercase $DEPLOY_FILE)  # 파일이 대문자이면 소문자로 변경
     DEPLOY_FILE=$(basename $APP_HOME/*.jar)       # 변경된 파일을 $DEPLOY_FILE 변수에 저장
     variable_func
@@ -59,9 +55,11 @@ lowercase_deploy_file()
   fi
 }
 
+# old 버전의 deploy 파일이 있으면 삭제
 remove_old_file()
 {
-  # old 버전의 배포 파일이 있으면 삭제한다.
+  OLD_FILE=$(ls -1 ${APP_HOME}/${SERVICE_NAME}*.jar 2> /dev/null | grep -v "$DEPLOY_FILE")            # 디렉토리의 파일 이름 목록을 배열에 저장 (ls -1)
+  OLD_FILE_COUNT=$(ls ${APP_HOME}/${SERVICE_NAME}*.jar 2> /dev/null | grep -v "$DEPLOY_FILE" | wc -l) # old 파일 개수 확인
   if [[ -n $OLD_FILE ]] && [[ $OLD_FILE_COUNT -gt 0 ]];then
     echo "OLD_FILE_COUNT=$OLD_FILE_COUNT"
     echo "remove old version"
@@ -73,9 +71,9 @@ remove_old_file()
   fi
 }
 
+# 실행 중인 컨테이너를 stop하고 delete한다.
 docker_conainer_stop_remove()
 {
-  # 실행 중인 컨테이너를 stop하고 delete한다.
   if [ $CONTAINER_ID ];then
     docker stop $CONTAINER_ID
     echo "docker rm $CONTAINER_ID"
@@ -83,15 +81,16 @@ docker_conainer_stop_remove()
   fi
 }
 
+# 기존 생성된 이미지 삭제
 docker_image_remove()
 {
-  # 기존 생성된 이미지 삭제
   if [ $IMAGE_ID ];then
     echo "docker rmi -f $IMAGE_ID"
     docker rmi -f $IMAGE_ID
   fi
 }
 
+# 도커 이미지 빌드
 docker_image_build()
 {
   echo "Docker build"
@@ -101,6 +100,7 @@ docker_image_build()
     --tag ${IMAGE_NAME}:${VERSION} ./
 }
 
+# 도커 컨테이너 시작
 docker_conainer_start()
 {
   echo "Run docker container"
@@ -122,5 +122,4 @@ main()
     rm $DEPLOY_FILE && echo "[ Successfully docker build and run ]"
 }
 
-# 인자값을 체크하고 대문자 파일명을 소문자로 변경한고 나면, main을 실행한다.
 main
